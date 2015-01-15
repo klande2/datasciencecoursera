6331abcdd669dc54b982f231afab018dd096031c
kman <klande2@yahoo.com>
2015-01-15 16:12:44 +0100

    fun

---
+++ plot3.R
@@ -0,0 +1,34 @@
+####################
+##### Peer Assignment 1: Plot 1
+####################
+
+#### Download the data to a temp file and uncompress
+fileUrl<-"https://archive.ics.uci.edu/ml/machine-learning-databases/00235/household_power_consumption.zip"
+temp <- tempfile("household_power_consumption", fileext=c("zip"))
+download.file(fileUrl,temp,mode="wb")
+raw<-unzip(temp,"household_power_consumption.txt")
+
+#### Create a data frame.  Used read.csv after trying csv2 and fread
+dmd <- read.csv(raw,header=TRUE,sep=";",nrows = 2075259,na.strings="?",stringsAsFactors=FALSE)
+
+### close the temp file
+unlink(temp)
+
+### Add column for DateTime
+dmd$DateTime <- as.POSIXct(paste(dmd$Date, dmd$Time), format="%d/%m/%Y %H:%M:%S")
+
+### Get the subset data
+subdmd<-subset(dmd,dmd$DateTime>='2007-02-01'&dmd$DateTime<'2007-02-03')
+
+### Reorder the columns and sort by DateTime
+subdmd<-subdmd[,c(10,3:9)]
+subdmdor<-subdmd[with(subdmd,order(subdmd$DateTime)),]
+
+#############
+### Create the png
+############
+### NB - this creates the code in your wd. e.g. getwd()
+
+png(file="plot2.png", width = 480, height = 480,bg="transparent")
+plot(subdmdor$DateTime,subdmdor$Global_active_power,type="l",ylab="Global Active Power (kilowatts)",xlab=" ")
+dev.off()
